5c8965192a3f7e14082f9b3dbe3e6f108d502e04
Kolia Horash <kolia28011994@gmail.com>
2018-06-11 23:12:42 +0300

    Kolia Horash
    Need to add data base. Problem with nethod double Click gridview

---
+++ TestProject/DB/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestProject.DB
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Description> Post { get; set; }
+    }
+}
---
+++ TestProject/Models/Description.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestProject.Models
+{
+    public class Description
+    {
+        public int ID { get; set; }
+        public DateTime DateCreate { get; set; }
+        public DateTime DateEndOfActuality { get; set; }
+        public string DescriptionOfObject { get; set; }
+        public string Category { get; set; }
+    }
+}
